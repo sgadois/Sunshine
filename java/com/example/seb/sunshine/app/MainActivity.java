6768dcc715329483863b1ebbb73c7b82bc011908
Sebastien Gadois <sgadois@gmail.com>
2016-08-24 11:58:18 +0200

    Initial commit

---
+++ java/com/example/seb/sunshine/app/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.seb.sunshine.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        /*
+        // Get the menu item.
+        MenuItem menuItem = menu.findItem(R.id.);
+        // Get the provider and hold onto it to set/change the share intent.
+        mShareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        // Set history different from the default before getting the action
+        // view since a call to MenuItem.getActionView() calls
+        // onCreateActionView() which uses the backing file name. Omit this
+        // line if using the default share history file is desired.
+        mShareActionProvider.setShareHistoryFileName("custom_share_history.xml");
+        */
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
